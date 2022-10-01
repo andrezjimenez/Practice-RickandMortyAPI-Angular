3f59060486cc17b391e815464ceb390bc477d5a5
AndrezJimenez <andru456@hotmail.com>
2022-09-30 22:21:43 -0500

    styles of characters and button more information

--- src/app/services/rickandmortyapi.service.ts
+++ src/app/services/rickandmortyapi.service.ts
@@ -18,7 +18,7 @@ export class RickandmortyapiService {
   ) { }
 
   getAllCharacter(){
-    return this.http.get<Character[]>(`${this.apiUrl}/character/1,2,3,4,5,6`);
+    return this.http.get<Character[]>(`${this.apiUrl}/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20`);
   }
 
 }
